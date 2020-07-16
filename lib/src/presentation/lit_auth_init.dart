804e238ae9415f70828e8c5c632ba8b9897d8a07
Peter Hayes <funwithflutter@gmail.com>
2020-07-16 15:46:38 +0700

    docs: description and examples for LitAuthInit

--- lib/src/presentation/lit_auth_init.dart
+++ lib/src/presentation/lit_auth_init.dart
@@ -7,6 +7,31 @@ import '../domain/auth/i_auth_facade.dart';
 import '../domain/auth/user.dart';
 import '../infrastructure/firebase_auth_facade.dart';
 
+/// The initialization for Firebase Authentication. This widget must be at the root of
+/// your application, above [MaterialApp] or [CupertinoApp].
+/// For example:
+/// ```dart
+/// class MyApp extends StatelessWidget {
+///  @override
+///  Widget build(BuildContext context) {
+///    return LitAuthInit(
+///      authProviders: AuthProviders(
+///        emailAndPassword: true,
+///        google: true,
+///        anonymous: true,
+///      ),
+///      child: MaterialApp(
+///        title: 'Material App',
+///        home: SplashScreen(),
+///      ),
+///    );
+///  }
+///}
+///```
+///{@end-tool}
+/// The enabled authentication providers should be specified with the
+/// [authProviders] parameter. By default only email and password authentication
+/// is enabled.
 class LitAuthInit extends StatelessWidget {
   const LitAuthInit({
     Key key,
@@ -15,7 +40,20 @@ class LitAuthInit extends StatelessWidget {
   })  : assert(child != null),
         super(key: key);
 
+  /// Specify the authentication providers that should be enabled for this
+  /// application. By default only email and password authentication is enabled.
+  ///
+  /// For example:
+  /// ```
+  ///AuthProviders(
+  ///  emailAndPassword: false, // enables Email and Password sign-in
+  ///  anonymous: true, // enables Anonymous sign-in
+  ///  google: true, // enables Google sign-in
+  ///)
+  ///```
+  ///{@end-tool}
   final AuthProviders authProviders;
+
   final Widget child;
 
   @override
