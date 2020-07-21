bc9b494d44e4c7fdd8e5d307745992f2a7121850
Peter Hayes <funwithflutter@gmail.com>
2020-07-22 00:02:59 +0700

    docs: add documentation

--- lib/src/presentation/widgets/sign_in_form.dart
+++ lib/src/presentation/widgets/sign_in_form.dart
@@ -3,6 +3,11 @@ import 'package:provider/provider.dart';
 
 import '../../application/auth/sign_in_handler/sign_in_handler_state.dart';
 
+/// A wrapper for a standard [Form] widget. It allows the [Form] to respond to
+/// error messages if the current email and password fields are not correct.
+///
+/// Should be used in conjunction with [EmailTextFormField] and
+/// [PasswordTextFormField]
 class SignInForm extends StatelessWidget {
   const SignInForm({Key key, @required this.child})
       : assert(child != null),
